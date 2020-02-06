50827c0a785bd53de57b00aca6b64dc2637a75d1
Deleu <deleugyn@gmail.com>
2020-02-06 20:04:34 +0100

    Setup buildspec with webpack

---
+++ webpack.aws.js
@@ -0,0 +1,33 @@
+const nodeExternals = require('webpack-node-externals');
+const path = require('path');
+
+module.exports = {
+  entry: ['./src/hello.js'],
+  target: 'node',
+  mode: 'production',
+  externals: [nodeExternals()],
+  module: {
+    rules: [
+      {
+        test: /\.tsx?$/,
+        use: 'ts-loader',
+        exclude: /node_modules/,
+      },
+      {
+        test: /\.jsx?$/,
+        include: __dirname,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+        },
+      },
+    ],
+  },
+  resolve: {
+    extensions: ['.tsx', '.ts', '.js', '.jsx'],
+  },
+  output: {
+    filename: 'bundle.js',
+    path: path.resolve(__dirname, 'dist'),
+  },
+};
