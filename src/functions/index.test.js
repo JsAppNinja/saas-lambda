95056ce04acab58ad116b867dd73a59edfe74769
Connor Barrett <connor.barrett.dev@gmail.com>
2020-02-09 11:04:36 -0600

    minor typo fix

--- src/functions/index.test.js
+++ src/functions/index.test.js
@@ -3,7 +3,7 @@ const { successResponse } = require('../utils/lambda-response');
 describe('index', () => {
   it('executes as expected', () => {
     const response = successResponse({
-      message: 'Your API request has excuted successfully!',
+      message: 'Your API request has executed successfully!',
       statusCode: 200,
     });
     expect(response.statusCode).toMatchSnapshot();
