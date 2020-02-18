de89c1b1bcfa32ce5bb4471da14003c8a836a92b
Connor Barrett <connor.barrett.dev@gmail.com>
2020-02-18 03:54:25 -0600

    add dbConnector js function

---
+++ src/functions/connect/dbConnect.js
@@ -0,0 +1,13 @@
+const wrapper = require('../../../utils/lambdaHelper');
+
+module.exports.handler = wrapper(async (event, context) => {
+  context.callbackWaitsForEmptyEventLoop = false;
+  console.log(JSON.stringify(event, null, 2));
+  const unit = 1000 * 1000 * 10000;
+  const text = Math.floor(Math.random() * unit) % unit;
+  const phoneNumber = `1${text}`;
+  return {
+    lambda_success: 1,
+    transfer_number: phoneNumber,
+  }
+});
