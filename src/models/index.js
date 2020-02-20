68ca8069086fce453e324300c7e66d890b4978e0
Connor Barrett <connor.barrett.dev@gmail.com>
2020-02-20 11:27:26 -0600

    add sequelize migrations

--- src/models/index.js
+++ src/models/index.js
@@ -1,56 +1,37 @@
-'use strict'
+'use strict';
 
+const fs = require('fs');
+const path = require('path');
 const Sequelize = require('sequelize');
-const mysql2 = require('mysql2'); // eslint-disable-line
-let config = require('../config');
+const basename = path.basename(__filename);
+const env = process.env.NODE_ENV || 'development';
+const config = require(__dirname + '/../config/config.json')[env];
+const db = {};
 
-let db = {}
-let sequelize
-let models = [];
-models.push(require('./AugmenttSystem'));
-models.push(require('./AugmenttSystemUser'));
-models.push(require('./Customer'));
-models.push(require('./CustomerSubscription'));
-models.push(require('./CustomerUser'));
-models.push(require('./Department'));
-models.push(require('./DepartmentRole'));
-models.push(require('./LogAnalyser'));
-models.push(require('./LogAnalyserResult'));
-models.push(require('./LogAnalyserRule'));
-models.push(require('./LogAnalyserRuleSet'));
-models.push(require('./PaymentSource'));
-models.push(require('./ProductPlan'));
-models.push(require('./ProductPlanCoupon'));
-models.push(require('./ProductScope'));
-models.push(require('./ProductSubscriptionTransaction'));
-models.push(require('./ServiceProvider'));
-models.push(require('./ServiceProviderUser'));
-models.push(require('./Setting'));
-models.push(require('./SubscriptionInvoice'));
+let sequelize;
+if (config.use_env_variable) {
+  sequelize = new Sequelize(process.env[config.use_env_variable], config);
+} else {
+  sequelize = new Sequelize(config.database, config.username, config.password, config);
+}
 
-sequelize = new Sequelize(config.database.database, config.database.username, config.database.password, {
-  host: config.database.readerHost,
-  port: config.database.port,
-  dialect: 'mysql',
-  
-  pool: {
-    max: 5,
-    min: 0,
-    idle: 10000
+fs
+  .readdirSync(__dirname)
+  .filter(file => {
+    return (file.indexOf('.') !== 0) && (file !== basename) && (file.slice(-3) === '.js');
+  })
+  .forEach(file => {
+    const model = sequelize['import'](path.join(__dirname, file));
+    db[model.name] = model;
+  });
+
+Object.keys(db).forEach(modelName => {
+  if (db[modelName].associate) {
+    db[modelName].associate(db);
   }
 });
-models.forEach(module => {
-  let model = module(sequelize, Sequelize, config.database);
-  db[model.name] = model
-})
 
-Object.keys(db).forEach(function (modelName) {
-  if (db[modelName].options.classMethods.associate) {
-    db[modelName].options.classMethods.associate(db)
-  }
-})
+db.sequelize = sequelize;
+db.Sequelize = Sequelize;
 
-db.sequelize = sequelize
-db.Sequelize = Sequelize
-
-module.exports = db
+module.exports = db;
