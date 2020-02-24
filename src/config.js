2e082c1132f13ef293aed7768bfb10489376d159
Connor Barrett <connor.barrett.dev@gmail.com>
2020-02-24 10:16:58 -0600

    models index update

--- src/config.js
+++ src/config.js
@@ -1,12 +1,11 @@
 const config = {
   database: {
-    readerHost     : process.env.DB_RD_HOST,
-    writerHost: process.env.DB_WR_HOST,
-    host: process.env.DB_HOST,
+    readerHost     : process.env.DB_RD_HOST || '127.0.0.1',
     port: 3306,
-    username     : process.env.DB_USERNAME,
-    password : process.env.DB_PASSWORD,
-    database : process.env.DB_NAME || 'connectFlow',
+    username     : process.env.DB_USERNAME || 'root',
+    password : process.env.DB_PASSWORD || null,
+    database : process.env.DB_NAME || 'augmentt_dev',
+    dialect  : process.env.DB_DIALECT || 'mysql',
   }
 };
 
