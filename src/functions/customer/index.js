aa73d808c8f33ffa7c79a4ad95e9bcf123cd6651
Connor Barrett <connor.barrett.dev@gmail.com>
2020-02-27 16:50:54 -0600

    customer module API endpoints update

--- src/functions/customer/index.js
+++ src/functions/customer/index.js
@@ -1,18 +1,19 @@
-// const metaHandler = require('./handlers/meta').handler;
-const createCustomer = require('./handlers/createCustomer').handler;
-const listCustomer = require('./handlers/listCustomer').handler;
 const ACTION = require('./constants');
+const customerHandler = require('./handlers/customer').handler;
+const customerSettingHandler = require('./handlers/customerSetting').handler;
+const subscriptionHandler = require('./handlers/subscription').handler;
+const subscriptionCouponHandler = require('./handlers/subscriptionCoupon').handler;
 
 module.exports.handler = async (event, context) => {
-  // Action Code Definition
   switch (event.action) {
-    case ACTION.SYNC_META:
-      // return metaHandler(event, context);
-      return {};
+    case ACTION.CUSTOMER_ROOT:
+      return customerHandler(event, context);
+    case ACTION.CUSTOMER_SETTING:
+      return customerSettingHandler(event, context);
     case ACTION.CREATE_CUSTOMER:
-      return createCustomer(event, context);
+      return subscriptionHandler(event, context);
     case ACTION.LIST_CUSTOMER:
-      return listCustomer(event, context);
+      return subscriptionCouponHandler(event, context);
     default:
       return {};
   }
