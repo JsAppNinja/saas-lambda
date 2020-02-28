b83f0e15ec33a76a3c3b4a70b715f465d90efc7c
Connor Barrett <connor.barrett.dev@gmail.com>
2020-02-28 16:47:13 -0600

    customer index function update

--- src/functions/customer/index.js
+++ src/functions/customer/index.js
@@ -1,9 +1,17 @@
 const UrlPattern = require('url-pattern');
 
 const customerHandler = require('./handlers/customer').handler;
+const customersHandler = require('./handlers/customers').handler;
 const customerSettingHandler = require('./handlers/customerSetting').handler;
-const subscriptionHandler = require('./handlers/subscription').handler;
-const subscriptionCouponHandler = require('./handlers/subscriptionCoupon').handler;
+const customerSettingsHandler = require('./handlers/customerSettings').handler;
+const customerSubscriptionHandler = require('./handlers/customerSubscription').handler;
+const customerSubscriptionsHandler = require('./handlers/customerSubscriptions').handler;
+const customerSubscriptionCouponHandler = require('./handlers/customerSubscriptionCoupon').handler;
+const customerSubscriptionCouponsHandler = require('./handlers/customerSubscriptionCoupons').handler;
+const customerUserHandler = require('./handlers/customerUser').handler;
+const customerUsersHandler = require('./handlers/customerUsers').handler;
+const customerUserSettingHandler = require('./handlers/customerUserSetting').handler;
+const customerUserSettingsHandler = require('./handlers/customerUserSettings').handler;
 
 const ACTION = require('./constants');
 const ROUTES = require('./routes');
@@ -21,16 +29,17 @@ module.exports.handler = async (event, context) => {
     input: event,
   });
 
-  switch (event.action) {
-    case ACTION.CUSTOMER_ROOT:
-      return customerHandler(event, context);
-    case ACTION.CUSTOMER_SETTING:
-      return customerSettingHandler(event, context);
-    case ACTION.CREATE_CUSTOMER:
-      return subscriptionHandler(event, context);
-    case ACTION.LIST_CUSTOMER:
-      return subscriptionCouponHandler(event, context);
-    default:
-      return successResponse;
-  }
+  // switch (event.action) {
+  //   case ACTION.CUSTOMER_ROOT:
+  //     return customerHandler(event, context);
+  //   case ACTION.CUSTOMER_SETTING:
+  //     return customerSettingHandler(event, context);
+  //   case ACTION.CREATE_CUSTOMER:
+  //     return subscriptionHandler(event, context);
+  //   case ACTION.LIST_CUSTOMER:
+  //     return subscriptionCouponHandler(event, context);
+  //   default:
+  //     return successResponse;
+  // }
+  return successResponse;
 };
