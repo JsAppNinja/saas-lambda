c58bd7ff5a49f2d59cc3f609c025b0b672397c7f
Connor Barrett <connor.barrett.dev@gmail.com>
2020-02-28 16:38:20 -0600

    add customer module functions, update customer index lambda and routes

--- src/functions/customer/index.js
+++ src/functions/customer/index.js
@@ -1,19 +1,26 @@
+const UrlPattern = require('url-pattern');
+
 const customerHandler = require('./handlers/customer').handler;
 const customerSettingHandler = require('./handlers/customerSetting').handler;
 const subscriptionHandler = require('./handlers/subscription').handler;
 const subscriptionCouponHandler = require('./handlers/subscriptionCoupon').handler;
 
 const ACTION = require('./constants');
+const ROUTES = require('./routes');
+
 const { successResponse } = require('../../utils/lambda-response');
 
 module.exports.handler = async (event, context) => {
   console.log('=== event log ===', event); // eslint-disable-line no-console
+
   const path = event.path;
   const httpMethod = event.httpMethod;
-  const response = successResponse({
+
+  const successResponse = successResponse({
     message: 'Go Serverless! Your function executed successfully!',
     input: event,
   });
+
   switch (event.action) {
     case ACTION.CUSTOMER_ROOT:
       return customerHandler(event, context);
@@ -24,6 +31,6 @@ module.exports.handler = async (event, context) => {
     case ACTION.LIST_CUSTOMER:
       return subscriptionCouponHandler(event, context);
     default:
-      return response;
+      return successResponse;
   }
 };
