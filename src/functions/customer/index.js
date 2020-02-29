351b45e85c20fe5eb48b061170c10aac3d24b39e
Connor Barrett <connor.barrett.dev@gmail.com>
2020-02-29 06:13:56 -0600

    add custom options into UrlPattern

--- src/functions/customer/index.js
+++ src/functions/customer/index.js
@@ -5,18 +5,18 @@ const ROUTES = require('./routes');
 
 const { successResponse, ResourceNotFoundResponse } = require('../../utils/lambda-response');
 
-// const customerHandler = require('./handlers/customer').handler;
-// const customersHandler = require('./handlers/customers').handler;
-// const customerSettingHandler = require('./handlers/customerSetting').handler;
-// const customerSettingsHandler = require('./handlers/customerSettings').handler;
-// const customerSubscriptionHandler = require('./handlers/customerSubscription').handler;
-// const customerSubscriptionsHandler = require('./handlers/customerSubscriptions').handler;
-// const customerSubscriptionCouponHandler = require('./handlers/customerSubscriptionCoupon').handler;
-// const customerSubscriptionCouponsHandler = require('./handlers/customerSubscriptionCoupons').handler;
-// const customerUserHandler = require('./handlers/customerUser').handler;
-// const customerUsersHandler = require('./handlers/customerUsers').handler;
-// const customerUserSettingHandler = require('./handlers/customerUserSetting').handler;
-// const customerUserSettingsHandler = require('./handlers/customerUserSettings').handler;
+const customerHandler = require('./handlers/customer').handler;
+const customersHandler = require('./handlers/customers').handler;
+const customerSettingHandler = require('./handlers/customerSetting').handler;
+const customerSettingsHandler = require('./handlers/customerSettings').handler;
+const customerSubscriptionHandler = require('./handlers/customerSubscription').handler;
+const customerSubscriptionsHandler = require('./handlers/customerSubscriptions').handler;
+const customerSubscriptionCouponHandler = require('./handlers/customerSubscriptionCoupon').handler;
+const customerSubscriptionCouponsHandler = require('./handlers/customerSubscriptionCoupons').handler;
+const customerUserHandler = require('./handlers/customerUser').handler;
+const customerUsersHandler = require('./handlers/customerUsers').handler;
+const customerUserSettingHandler = require('./handlers/customerUserSetting').handler;
+const customerUserSettingsHandler = require('./handlers/customerUserSettings').handler;
 
 module.exports.handler = async (event, context) => {
   console.log('=== event log ===', event); // eslint-disable-line no-console
@@ -36,9 +36,11 @@ module.exports.handler = async (event, context) => {
 
   const routes = Object.values(ROUTES);
   const actions = Object.keys(ROUTES);
-
+  const options = {
+    segmentNameCharset: 'a-zA-Z0-9_',
+  };
   const patternIndex = routes.findIndex(route => {
-    const pattern = new UrlPattern(route);
+    const pattern = new UrlPattern(route, options);
     const result = pattern.match(path);
     console.log('Testing patterns =>', pattern, result); // eslint-disable-line no-console
     return result != null;
@@ -48,35 +50,35 @@ module.exports.handler = async (event, context) => {
   if (patternIndex < 0 ) {
     return resourceNotFoundResponse;
   } else {
-    // const event = actions[patternIndex];
-    //   switch (event) {
-    //     case ACTION.CUSTOMERS:
-    //       return customersHandler(event, context);
-    //     case ACTION.CUSTOMER:
-    //       return customerHandler(event, context);
-    //     case ACTION.CUSTOMER_SETTINGS:
-    //       return customerSettingsHandler(event, context);
-    //     case ACTION.CUSTOMER_SETTING:
-    //       return customerSettingHandler(event, context);
-    //     case ACTION.CUSTOMER_USERS:
-    //       return customerUsersHandler(event, context);
-    //     case ACTION.CUSTOMER_USER:
-    //       return customerUserHandler(event, context);
-    //     case ACTION.CUSTOMER_USER_SETTINGS:
-    //       return customerUserSettingsHandler(event, context);
-    //     case ACTION.CUSTOMER_USER_SETTING:
-    //       return customerUserSettingHandler(event, context);
-    //     case ACTION.CUSTOMER_SUBSCRIPTIONS:
-    //       return customerSubscriptionsHandler(event, context);
-    //     case ACTION.CUSTOMER_SUBSCRIPTION:
-    //       return customerSubscriptionHandler(event, context);  
-    //     case ACTION.CUSTOMER_SUBSCRIPTION_COUPONS:
-    //       return customerSubscriptionCouponsHandler(event, context); 
-    //     case ACTION.CUSTOMER_SUBSCRIPTION_COUPON:
-    //       return customerSubscriptionCouponHandler(event, context); 
-    //     default:
-    //       return successResponse;
-    //   }
+    const event = actions[patternIndex];
+    // switch (event) {
+    //   case ACTION.CUSTOMERS:
+    //     return customersHandler(event, context);
+    //   case ACTION.CUSTOMER:
+    //     return customerHandler(event, context);
+    //   case ACTION.CUSTOMER_SETTINGS:
+    //     return customerSettingsHandler(event, context);
+    //   case ACTION.CUSTOMER_SETTING:
+    //     return customerSettingHandler(event, context);
+    //   case ACTION.CUSTOMER_USERS:
+    //     return customerUsersHandler(event, context);
+    //   case ACTION.CUSTOMER_USER:
+    //     return customerUserHandler(event, context);
+    //   case ACTION.CUSTOMER_USER_SETTINGS:
+    //     return customerUserSettingsHandler(event, context);
+    //   case ACTION.CUSTOMER_USER_SETTING:
+    //     return customerUserSettingHandler(event, context);
+    //   case ACTION.CUSTOMER_SUBSCRIPTIONS:
+    //     return customerSubscriptionsHandler(event, context);
+    //   case ACTION.CUSTOMER_SUBSCRIPTION:
+    //     return customerSubscriptionHandler(event, context);  
+    //   case ACTION.CUSTOMER_SUBSCRIPTION_COUPONS:
+    //     return customerSubscriptionCouponsHandler(event, context); 
+    //   case ACTION.CUSTOMER_SUBSCRIPTION_COUPON:
+    //     return customerSubscriptionCouponHandler(event, context); 
+    //   default:
+    //     return requestSuccessResponse;
+    // }
     return requestSuccessResponse;
   }
 };
