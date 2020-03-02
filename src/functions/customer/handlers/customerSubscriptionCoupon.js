d9a1d47ec706e3fc20b243b90da4214f19dc8967
Connor Barrett <connor.barrett.dev@gmail.com>
2020-03-02 08:59:47 -0600

    subscription coupons module update

--- src/functions/customer/handlers/customerSubscriptionCoupon.js
+++ src/functions/customer/handlers/customerSubscriptionCoupon.js
@@ -1,10 +1,50 @@
-const { successResponse } = require('../../../utils/lambda-response');
+const { successResponse, InvalidResponse } = require('../../../utils/lambda-response');
+const models = require('../../../models');
+const Customer = models.customers;
+const Subscription = models.subscriptions;
+const CustomerUser = models.customer_users;
+const Setting = models.settings;
 
 module.exports.handler = async event => {
-  const response = successResponse({
-    message: 'We are updating one subscription coupon info for your requested customer!',
-    input: event,
-  });
-
-  return response;
+  const method = httpMethod;
+  switch (method) {
+    case 'GET':
+      const customerId = event.customerId;
+      // const oneCustomer = await Customer.findByPk(customerId);
+      const response1 = successResponse({
+        message: 'We are getting your requested subscription coupon information!',
+        input: event,
+        // content: oneCustomer,
+      });
+      return response1;
+    case 'POST':
+      const result = await Customer.update(
+        {title: req.body.title},
+        {returning: true, where: {id: req.params.id} }
+      );
+      const response2 = successResponse({
+        message: 'We are updating your requested subscription coupon information!',
+        input: event,
+        content: result,
+      });
+      return response2;
+    case 'DELETE':
+      const isDeleted = await Customer.destroy({
+        where: {
+          id: customerId
+        }
+       });
+      const response3 = successResponse({
+        message: 'We are deleting your requested subscription coupon information!',
+        input: event,
+        content: isDeleted,
+      });
+      return response3;
+    default:
+      const generalResponse = successResponse({
+        message: 'We are handling your requested subscription coupon information!',
+        input: event,
+      });
+      return generalResponse;
+  }
 };
