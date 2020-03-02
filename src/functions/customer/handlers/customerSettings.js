fa924e81b4e473463d95bf8edd5b88232aaf08d1
Connor Barrett <connor.barrett.dev@gmail.com>
2020-03-02 08:44:00 -0600

    Customer Setting lambda functions update

--- src/functions/customer/handlers/customerSettings.js
+++ src/functions/customer/handlers/customerSettings.js
@@ -1,10 +1,38 @@
-const { successResponse } = require('../../../utils/lambda-response');
+const { successResponse, InvalidResponse } = require('../../../utils/lambda-response');
+const models = require('../../../models');
+const Customer = models.customers;
+const Subscription = models.subscriptions;
+const CustomerUser = models.customer_users;
+const Setting = models.settings;
 
 module.exports.handler = async event => {
-  const response = successResponse({
-    message: 'We are updating the settings info for your requested customer!',
-    input: event,
-  });
-
-  return response;
+  const method = httpMethod;
+  switch (method) {
+    case 'GET':
+      const customerId = event.customerId;
+      // const oneCustomer = await Customer.findByPk(customerId);
+      const response1 = successResponse({
+        message: 'We are getting your requested customer settings information!',
+        input: event,
+        // content: oneCustomer,
+      });
+      return response1;
+    case 'PUT':
+      const result = await Customer.post(
+        {title: req.body.title},
+        {returning: true, where: {id: req.params.id} }
+      );
+      const response2 = successResponse({
+        message: 'We are updating your requested customer settings information!',
+        input: event,
+        content: result,
+      });
+      return response2;
+    default:
+      const generalResponse = successResponse({
+        message: 'We are handling your requested customer setting information!',
+        input: event,
+      });
+      return generalResponse;
+  }
 };
