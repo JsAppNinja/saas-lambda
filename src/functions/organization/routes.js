0c3b5c48c3b083121cc28cb6519bef60cfa4889f
Connor Barrett <connor.barrett.dev@gmail.com>
2020-03-03 04:19:51 -0600

    Organization lambda create for organization models

---
+++ src/functions/organization/routes.js
@@ -0,0 +1,5 @@
+module.exports = {
+  ORGANIZATIONS: '/augmentt/organization',
+  ORGANIZATION: '/augmentt/organization/:organizationId',
+  ORGANIZATION_CUSTOMER: '/augmentt/customer/:organizationId/customer',
+};
