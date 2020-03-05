a05c9711ae42184b46412a6c1bb0f12878f633e5
Connor Barrett <connor.barrett.dev@gmail.com>
2020-03-05 05:40:18 -0600

    index module update

--- src/functions/index.js
+++ src/functions/index.js
@@ -1,5 +1,38 @@
-const { successResponse } = require('../utils/lambda-response');
-const organizationModuleHandler = require('./organization').handler;
+const UrlPattern = require('url-pattern');
 
-module.exports.handler = async (event, context) =>
-  organizationModuleHandler(event, context);
+const ACTION = require('./constants');
+const ROUTES = require('./routes');
+
+const { ResourceNotFoundResponse } = require('../utils/lambda-response');
+const organizationModuleHandler = require('./organization').handler;
+const customerModuleHandler = require('./customer').handler;
+
+module.exports.handler = async (event, context) => {
+  const { path } = event;
+
+  const resourceNotFoundResponse = ResourceNotFoundResponse({
+    message: 'The requested route is invalid. Please input the correct URL!',
+    input: event,
+  });
+
+  const routes = Object.values(ROUTES);
+  const patternIndex = routes.findIndex(route => {
+    const pattern = new UrlPattern(route);
+    const result = pattern.match(path);
+    return result != null;
+  });
+
+  if (patternIndex < 0) {
+    return resourceNotFoundResponse;
+  }
+  const actions = Object.keys(ROUTES);
+  const eventAction = actions[patternIndex];
+  switch (eventAction) {
+    case ACTION.ORGANIZATION:
+      return organizationModuleHandler(event, context);
+    case ACTION.CUSTOMER:
+      return customerModuleHandler(event, eventRoute);
+    default:
+      return resourceNotFoundResponse;
+  }
+};
