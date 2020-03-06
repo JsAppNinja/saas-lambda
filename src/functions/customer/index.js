36dbe54898f1e47c6155303ad17b6a66016c72dd
Connor Barrett <connor.barrett.dev@gmail.com>
2020-03-05 18:57:30 -0600

    customer module remove one API endpoint

--- src/functions/customer/index.js
+++ src/functions/customer/index.js
@@ -3,20 +3,28 @@ const UrlPattern = require('url-pattern');
 const ACTION = require('./constants');
 const ROUTES = require('./routes');
 
-const { successResponse, ResourceNotFoundResponse } = require('../../utils/lambda-response');
+const {
+  successResponse,
+  ResourceNotFoundResponse,
+} = require('../../utils/lambda-response');
 
 const customerHandler = require('./handlers/customer').handler;
-const customersHandler = require('./handlers/customers').handler;
 const customerSettingHandler = require('./handlers/customerSetting').handler;
 const customerSettingsHandler = require('./handlers/customerSettings').handler;
-const customerSubscriptionHandler = require('./handlers/customerSubscription').handler;
-const customerSubscriptionsHandler = require('./handlers/customerSubscriptions').handler;
-const customerSubscriptionCouponHandler = require('./handlers/customerSubscriptionCoupon').handler;
-const customerSubscriptionCouponsHandler = require('./handlers/customerSubscriptionCoupons').handler;
+const customerSubscriptionHandler = require('./handlers/customerSubscription')
+  .handler;
+const customerSubscriptionsHandler = require('./handlers/customerSubscriptions')
+  .handler;
+const customerSubscriptionCouponHandler = require('./handlers/customerSubscriptionCoupon')
+  .handler;
+const customerSubscriptionCouponsHandler = require('./handlers/customerSubscriptionCoupons')
+  .handler;
 const customerUserHandler = require('./handlers/customerUser').handler;
 const customerUsersHandler = require('./handlers/customerUsers').handler;
-const customerUserSettingHandler = require('./handlers/customerUserSetting').handler;
-const customerUserSettingsHandler = require('./handlers/customerUserSettings').handler;
+const customerUserSettingHandler = require('./handlers/customerUserSetting')
+  .handler;
+const customerUserSettingsHandler = require('./handlers/customerUserSettings')
+  .handler;
 
 module.exports.handler = async (event, context) => {
   console.log('=== event log ===', event); // eslint-disable-line no-console
@@ -36,7 +44,7 @@ module.exports.handler = async (event, context) => {
 
   const routes = Object.values(ROUTES);
   const actions = Object.keys(ROUTES);
-  const patternIndex = routes.findIndex((route) => {
+  const patternIndex = routes.findIndex(route => {
     const pattern = new UrlPattern(route);
     const result = pattern.match(path);
     return result != null;
@@ -48,8 +56,6 @@ module.exports.handler = async (event, context) => {
   }
   const eventAction = actions[patternIndex];
   switch (eventAction) {
-    case ACTION.CUSTOMERS:
-      return customersHandler(event, httpMethod);
     case ACTION.CUSTOMER:
       return customerHandler(event, httpMethod);
     case ACTION.CUSTOMER_SETTINGS:
--- src/functions/customer/routes.js
+++ src/functions/customer/routes.js
@@ -1,14 +1,17 @@
 module.exports = {
-  CUSTOMERS: '/augmentt/customer',
   CUSTOMER: '/augmentt/customer/:customerId',
   CUSTOMER_SETTINGS: '/augmentt/customer/:customerId/setting',
   CUSTOMER_SETTING: '/augmentt/customer/:customerId/setting/:settingId',
   CUSTOMER_USERS: '/augmentt/customer/:customerId/user',
   CUSTOMER_USER: '/augmentt/customer/:customerId/user/:userId',
   CUSTOMER_USER_SETTINGS: '/augmentt/customer/:customerId/user/:userId/setting',
-  CUSTOMER_USER_SETTING: '/augmentt/customer/:customerId/user/:userId/setting/:settingId',
+  CUSTOMER_USER_SETTING:
+    '/augmentt/customer/:customerId/user/:userId/setting/:settingId',
   CUSTOMER_SUBSCRIPTIONS: '/augmentt/customer/:customerId/subscription',
-  CUSTOMER_SUBSCRIPTION: '/augmentt/customer/:customerId/subscription/:subscriptionId',
-  CUSTOMER_SUBSCRIPTION_COUPONS: '/augmentt/customer/:customerId/subscription/:subscriptionId/coupon',
-  CUSTOMER_SUBSCRIPTION_COUPON: '/augmentt/customer/:customerId/subscription/:subscriptionId/coupon/:couponId',
+  CUSTOMER_SUBSCRIPTION:
+    '/augmentt/customer/:customerId/subscription/:subscriptionId',
+  CUSTOMER_SUBSCRIPTION_COUPONS:
+    '/augmentt/customer/:customerId/subscription/:subscriptionId/coupon',
+  CUSTOMER_SUBSCRIPTION_COUPON:
+    '/augmentt/customer/:customerId/subscription/:subscriptionId/coupon/:couponId',
 };
